e9a07830de08eac07c0affdc95a764b5f58b2ace
Rob Muhlestein <rob@rwx.gg>
2022-11-28 00:10:35 -0500

    Make go the default (and good the alias)

--- cmd.go
+++ cmd.go
@@ -6,9 +6,9 @@ import (
 )
 
 var Cmd = &Z.Cmd{
-	Name:      `good`,
-	Aliases:   []string{`go`},
-	Version:   `v0.1.1`,
+	Name:      `go`,
+	Aliases:   []string{`good`},
+	Version:   `v0.1.2`,
 	Summary:   `go helper commands and tasks`,
 	Copyright: `Copyright 2022 Robert S Muhlestein`,
 	License:   `Apache-2.0`,
